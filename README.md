6f8d7268385be4edd481a390011bed51c4a6c4c2
Farshad Mozafari <farshadmozafari.64@gmail.com>
2024-12-30 20:15:47 +0330

    Customizing Database Schema

---
+++ README.md
@@ -0,0 +1,4 @@
+# MOSH HAMEDANI DJANGO COURSE
+
+hello
+in this repository i've made commited each lesson in the django course from mosh hamedani instructor
---
+++ tags/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TagItem(models.Model):
+    """What tag applied to what object"""
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # type(Product,vudeo,article)
+    # ID
+    content_type=models.ForeignKey(ContentType,on_delete=models.CASCADE)
+    object_id=models.PositiveIntegerField()
+    content_object=GenericForeignKey
+    
+    
