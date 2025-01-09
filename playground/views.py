59892a92220638a57641554ebc5c5ad3773c8d21
Farshad Mozafari <farshadmozafari.64@gmail.com>
2025-01-09 10:34:58 +0330

    25- Transactions

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product
+from store.models import Order, OrderItem, Customer, Product
 from tags.models import TagItem
+from django.db import transaction
 
 
 def say_hello(request):
-    queryset = TagItem.objects.get_tags_for(Product, 1)
-    return render(request, 'playground/hello.html', {'name': 'farshad', 'result': queryset})
+
+    with transaction.atomic():
+        order = Order()
+        order.Customer_id = 1
+        order.save()
+
+        item = OrderItem()
+        item.order = order
+        item.product_id = -1
+        item.quantity = 1
+        item.unit_price = 10
+        item.save()
+    return render(request, 'playground/hello.html', {'name': 'farshad', })
